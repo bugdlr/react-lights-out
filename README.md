eb3abf8e82f274ec3d6c2a553428e4c2c33ff718
Hannah De Los Reyes <htdelosreyes8@gmail.com>
2019-06-05 11:42:08 -0700

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# react-lights-out
---
+++ precache-manifest.425ac7cd77dc61b64962684dbc60f250.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1764a79bcd54ed66a092",
+    "url": "/react-lights-out/static/js/runtime~main.1764a79b.js"
+  },
+  {
+    "revision": "f521634f9fe61b47ec1b",
+    "url": "/react-lights-out/static/js/main.f521634f.chunk.js"
+  },
+  {
+    "revision": "207e5d056e3105dcde38",
+    "url": "/react-lights-out/static/js/1.207e5d05.chunk.js"
+  },
+  {
+    "revision": "f521634f9fe61b47ec1b",
+    "url": "/react-lights-out/static/css/main.06ed1e38.chunk.css"
+  },
+  {
+    "revision": "817adace3d6313cf3d558e54ed900dca",
+    "url": "/react-lights-out/index.html"
+  }
+];
